caa55a920d2ddec92c76d3b40e8f0a175f07ee1d
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:33:06 +0100

    Pawn cannot move if square not empty

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -3,6 +3,14 @@ import Player from '../player';
 import Board from '../board';
 import Square from '../square';
 
+function isValid(cell: number, step: number): boolean {
+    return (cell + step < 8 && cell + step >= 0);
+}
+
+function isEmpty(row: number, col: number, board: Board): boolean {
+    return (!(board.getPiece(new Square(row, col)) instanceof Piece));
+}
+
 export default class Pawn extends Piece {
     public constructor(player: Player) {
         super(player);
@@ -14,18 +22,22 @@ export default class Pawn extends Piece {
 
         if (this.player === Player.WHITE) {
             // Make white pawn move one square up
-            availableMoves.push(new Square(Math.min(8, position.row + 1), position.col));
+            if (isValid(position.row, 1) && isEmpty(position.row + 1, position.col, board)) {
+                availableMoves.push(new Square(position.row + 1, position.col));
 
-            // If first move, make white pawn move two squares up
-            if (position.row === 1)
-                availableMoves.push(new Square(Math.min(8, position.row + 2), position.col));
+                // If first move, make white pawn move two squares up
+                if (position.row === 1 && isValid(position.row, 2) && isEmpty(position.row + 2, position.col, board))
+                    availableMoves.push(new Square(position.row + 2, position.col));
+            }
         } else {
             // Make black pawn move one square down
-            availableMoves.push(new Square(Math.max(0, position.row - 1), position.col));
+            if (isValid(position.row, -1)  && isEmpty(position.row - 1, position.col, board)) {
+                availableMoves.push(new Square(position.row - 1, position.col));
 
-            // If first move, make black pawn move two squares down
-            if (position.row === 6)
-                availableMoves.push(new Square(Math.max(0, position.row - 2), position.col));
+                // If first move, make black pawn move two squares down
+                if (position.row === 6 && isValid(position.row, -2)  && isEmpty(position.row - 2, position.col, board))
+                    availableMoves.push(new Square(position.row - 2, position.col));
+            }
         }
 
         return availableMoves;
