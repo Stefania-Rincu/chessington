c5a8b372defa8e08b561f6377f351e4180a0e394
Stefania Rincu <Stefania.Rincu@softwire.com>
2025-07-07 12:01:20 +0300

    Add pawn movements

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,11 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const position: Square = board.findPiece(this);
+
+        if (this.player === Player.WHITE)
+            return [new Square(Math.min(8, position.row + 1), position.col)];
+        else
+            return [new Square(Math.max(0, position.row - 1), position.col)];
     }
 }
