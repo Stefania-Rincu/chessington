b8fb8bc048aa851fce65e2cda8364a280d201a88
Stefania Rincu <Stefania.Rincu@softwire.com>
2025-07-07 13:36:03 +0300

    Add movement for bishop

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,24 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const position: Square = board.findPiece(this);
+
+        // Define list of forward and backward moves
+        const forwardMoves: Square[] = [];
+        const backwardMoves: Square[] = [];
+
+        // Complete moves diagonally
+        for (let i: number = 0; i < 7; i ++) {
+            const j: number = Math.abs(position.row - position.col) + i;
+            const k: number = position.col + position.row - i;
+
+            // Check indices to not access invalid positions
+            if (i != position.row && j != position.col && j < 8 && j >= 0)
+                forwardMoves.push(new Square(i, j));
+            if (i != position.row && k != position.col && k < 8 && k >= 0)
+                backwardMoves.push(new Square(i, k));
+        }
+
+        return forwardMoves.concat(backwardMoves);
     }
 }
