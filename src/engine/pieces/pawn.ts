8f4fbed07322250f0d75e2a6f3eca295a78da806
Stefania Rincu <Stefania.Rincu@softwire.com>
2025-07-07 17:21:04 +0300

    Make pawn move diagonally

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from '../square';
+import King from './king';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -11,6 +12,7 @@ export default class Pawn extends Piece {
     public getAvailableMoves(board: Board) {
         const position: Square = board.findPiece(this);
         const availableMoves: Square[] = [];
+        let direction: number[][];
 
         if (this.player === Player.WHITE) {
             // Make white pawn move one square up
@@ -21,6 +23,8 @@ export default class Pawn extends Piece {
                 if (position.row === 1 && this.isNotOutside(position.row + 2) && this.isSquareEmpty(board, position.row + 2, position.col))
                     availableMoves.push(new Square(position.row + 2, position.col));
             }
+
+            direction = [[1, -1], [1, 1]];
         } else {
             // Make black pawn move one square down
             if (this.isNotOutside(position.row - 1) && this.isSquareEmpty(board, position.row - 1, position.col)) {
@@ -30,7 +34,20 @@ export default class Pawn extends Piece {
                 if (position.row === 6 && this.isNotOutside(position.row - 2) && this.isSquareEmpty(board, position.row -2, position.col))
                     availableMoves.push(new Square(position.row - 2, position.col));
             }
+
+            direction = [[-1, -1], [-1, 1]];
         }
+
+        for (const dir of direction)
+            // If the last cell was not empty and still inside the board
+            if (this.isNotOutside(position.row + dir[0]) && this.isNotOutside(position.col + dir[1]) && ! this.isSquareEmpty(board, position.row + dir[0], position.col + dir[1])) {
+                // Get the piece on the square
+                const otherPiece = board.getPiece(new Square(position.row + dir[0], position.col + dir[1]));
+
+                // If from other player and not king, take it
+                if (otherPiece?.player != this.player && ! (otherPiece instanceof King))
+                    availableMoves.push(new Square(position.row + dir[0], position.col + dir[1]));
+            }
 
         return availableMoves;
     }
