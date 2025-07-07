a8dfec40c0fed80e64b63f380f67817adcdca3d2
Stefania Rincu <Stefania.Rincu@softwire.com>
2025-07-07 17:13:16 +0300

    Add constraints for king

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -14,14 +14,27 @@ export default class King extends Piece {
 
         // Define possible movements (directions)
         const directions: number[][] = [[-1, 0], [1, 0], [0, 1], [0, -1], [-1, 1], [1, -1], [1, 1], [-1, -1]];
-
-        // Define list to store movements
         const availableMoves: Square[] = [];
 
         // Construct the list of possible movement positions
-        for (const dir of directions)
-            if (this.isNotOutside(position.row + dir[0]) && this.isNotOutside(position.col + dir[1]))
-                availableMoves.push(new Square(position.row + dir[0], position.col + dir[1]));
+        for (const dir of directions) {
+            let row: number = position.row;
+            let col: number = position.col;
+
+            // Add constraints to respect rules
+            if (this.isNotOutside(row + dir[0]) && this.isNotOutside(col + dir[1]) && this.isSquareEmpty(board, row + dir[0], col + dir[1]))
+            availableMoves.push(new Square(row + dir[0], col + dir[1]));
+
+            // If the cell is not empty, but it's inside the board
+            if (this.isNotOutside(row + dir[0]) && this.isNotOutside(col + dir[1]) && !this.isSquareEmpty(board, row + dir[0], col + dir[1])) {
+                // Get the piece on the square
+                const otherPiece = board.getPiece(new Square(row + dir[0], col + dir[1]));
+
+                // If from other player and not king, take it
+                if (otherPiece?.player != this.player && !(otherPiece instanceof King))
+                    availableMoves.push(new Square(row + dir[0], col + dir[1]));
+            }
+        }
 
         return availableMoves;
     }
