3a252882f87fc1af97e5a09e39b635046e42ee2c
Stefania Rincu <Stefania.Rincu@softwire.com>
2025-07-07 12:35:12 +0300

    Add movement for rook

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,20 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const position: Square = board.findPiece(this);
+
+        // Define lists for horizontal and vertical movements
+        const availableMovesH: Square[] = [];
+        const availableMovesV: Square[] = [];
+
+        // Complete movements excluding just the square where the rook is placed
+        for (let i: number = 0; i < 8; i++) {
+            if (i != position.col)
+                availableMovesH.push(new Square(position.row, i));
+            if (i != position.row)
+                availableMovesV.push(new Square(i, position.col));
+        }
+
+        return availableMovesH.concat(availableMovesV);
     }
 }
