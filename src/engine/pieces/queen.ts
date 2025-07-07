80232dc25478c06ac8d04f653487ffc599456192
Stefania Rincu <Stefania.Rincu@softwire.com>
2025-07-07 16:02:02 +0300

    Queen cannot move if square not empty

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -3,6 +3,16 @@ import Player from '../player';
 import Board from '../board';
 import Square from "../square";
 
+// Function to check that a piece is not placed outside the board
+function isValid(cell: number, step: number): boolean {
+    return (cell + step < 8 && cell + step >= 0);
+}
+
+// Function that checks if a square is empty
+function isEmpty(row: number, col: number, board: Board): boolean {
+    return (!(board.getPiece(new Square(row, col)) instanceof Piece));
+}
+
 export default class Queen extends Piece {
     public constructor(player: Player) {
         super(player);
@@ -11,33 +21,20 @@ export default class Queen extends Piece {
     public getAvailableMoves(board: Board) {
         const position: Square = board.findPiece(this);
 
-        // Define lists for horizontal and vertical movements
-        const availableMovesH: Square[] = [];
-        const availableMovesV: Square[] = [];
+        const moves: number[][] = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [-1, -1], [-1, 1], [1, -1]];
+        const availableMoves: Square[] = [];
 
-        // Define list of forward and backward moves
-        const forwardMoves: Square[] = [];
-        const backwardMoves: Square[] = [];
+        for (const move of moves) {
+            let row: number = position.row;
+            let col: number = position.col;
 
-        // Complete moves literally and diagonally excluding just the square where the queen is placed
-        for (let i: number = 0; i < 8; i ++) {
-            // Horizontal and vertical moves
-            if (i != position.col)
-                availableMovesH.push(new Square(position.row, i));
-            if (i != position.row)
-                availableMovesV.push(new Square(i, position.col));
+            while (isValid(row, move[0]) && isValid(col, move[1]) && isEmpty(row + move[0], col + move[1], board)) {
+                row += move[0];
+                col += move[1];
 
-            // Diagonal moves
-            const j: number = Math.abs(position.row - position.col) + i;
-            const k: number = position.col + position.row - i;
-
-            // Check indices to not access invalid positions
-            if (i != position.row && j != position.col && j < 8 && j >= 0)
-                forwardMoves.push(new Square(i, j));
-            if (i != position.row && k != position.col && k < 8 && k >= 0)
-                backwardMoves.push(new Square(i, k));
+                availableMoves.push(new Square(row, col));
+            }
         }
-
-        return availableMovesH.concat(availableMovesV).concat(forwardMoves).concat(backwardMoves);
+        return availableMoves;
     }
 }
